c5c74274c5e4a2ebdea67c52b3a5dddb82fe7c64
maryam <maryam@localhost.localdomain>
2017-04-05 21:54:07 +0430

    user work with photo

--- app/User.php
+++ app/User.php
@@ -23,7 +23,13 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
     public function role(){
         return $this->belongsTo('App\Role');
     }
+
+    public function photo(){
+        return $this->belongsTo('App\Photo');
+    }
+
 }
